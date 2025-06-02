99892c5b8163c34fb852ab75a1463c8a8a25ef07
Martin Alarcón Huaiquimilla <m-alarcon.2003@hotmail.com>
2025-06-02 11:32:32 -0400

    Conexion con Supabase y codigo de pruebas
    el prisma seed.js es donde estan los datos de prueba, esos se pueden borrar(yo recomiendo dejarlos para ir haciendo pruebas desde un inicio)

--- lib/ui/views/app_bar.dart
+++ lib/ui/views/app_bar.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:sistema_acviis/constants/constants.dart';
 import 'package:sistema_acviis/ui/styles/app_colors.dart';
 
 class PersonalizedAppBar extends StatelessWidget implements PreferredSizeWidget {
