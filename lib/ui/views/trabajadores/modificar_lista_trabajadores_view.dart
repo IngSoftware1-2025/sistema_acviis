7b11ac310e1565280511e9c9d95d9c852bd2e8fa
benjotenks <b.elguetaperez@gmail.com>
2025-06-07 22:23:13 -0400

    Gane el cachipum

--- lib/ui/views/trabajadores/modificar_lista_trabajadores_view.dart
+++ lib/ui/views/trabajadores/modificar_lista_trabajadores_view.dart
@@ -15,7 +15,7 @@ class _ModificiarListaTrabajadoresViewState extends State<ModificarListaTrabajad
   @override
   Widget build(BuildContext context) {
     return PrimaryScaffold(
-      title: 'Modificar lista de trabajadores',
+      title: 'Modificando lista de trabajadores',
       body: Column(
         children: [
           ElevatedButton(
